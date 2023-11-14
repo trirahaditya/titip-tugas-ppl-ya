b265f385ee1fa4efdafe3ae17e4306204b1b8cad
gitaa.pragma <gita.pragma@gmail.com>
2023-11-14 11:02:41 +0800

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        
     }
 
     /**
@@ -24,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if (env('FORCE_HTTPS', true)) { // Default value should be false for local server
+            URL::forceScheme('https');
+        }
     }
 }
